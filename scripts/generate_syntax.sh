bfd67ec08b9bbf4d0afcd297398d9231f3122291
danomatika <danomatika@gmail.com>
2025-04-17 22:34:05 +0200

    generate_syntax: chek if ofxSyphon before generating

--- scripts/generate_syntax.sh
+++ scripts/generate_syntax.sh
@@ -3,10 +3,11 @@
 # generate Lua bindings
 #
 
+ADDONS=../../../../addons
 DEST=../doc/modules
 
 # path to lua_syntax script
-LUA=../../../../addons/ofxLua
+LUA=$ADDONS/ofxLua
 LUA_SYNTAX=$LUA/scripts/lua_syntax.py
 
 ###
@@ -19,13 +20,18 @@ mv $LUA/swig/*_lua_symbols.txt ../swig
 cd ../swig
 make symbols TARGET=osc INTERFACE=osc.i
 make symbols TARGET=loaf INTERFACE=loaf.i
-make symbols TARGET=syphon INTERFACE=syphon.i
 
 $LUA_SYNTAX of of_lua_symbols.txt
 $LUA_SYNTAX glm glm_lua_symbols.txt
 $LUA_SYNTAX osc osc_symbols.txt
 $LUA_SYNTAX loaf loaf_symbols.txt
-$LUA_SYNTAX syphon syphon_symbols.txt
+
+if [ -d $ADDONS/ofxSyphon ] ; then
+	make symbols TARGET=syphon INTERFACE=syphon.i
+	$LUA_SYNTAX syphon syphon_symbols.txt
+else
+	ehco "ofxSyphon not found, skipping..."
+fi
 
 # added by loaf.i
 echo "loaf.send" >> loaf_syntax.txt
